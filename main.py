a63bdd7aaed7101362359e54f11e508e8e9baf82
preethigutha <pratimabankar98.2022@gmail.com>
2023-12-05 20:10:51 -0500

    Creating main.py file

---
+++ main.py
@@ -0,0 +1,3 @@
+# Author Name: Pratima Bankar, Shiva Raj Harinath Reddy , Preethi Gutha
+# Date: 12/04/2023
+# Description: The program below depicts the main file where all the classes are initialized.
