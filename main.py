fd9b21b130919d9697786376169e7dcf7aef3bc9
pratima-bankar <148815365+pratima-bankar@users.noreply.github.com>
2023-12-05 20:11:36 -0500

    Delete main.py

--- main.py
+++
@@ -1,3 +0,0 @@
-# Author Name: Pratima Bankar, Shiva Raj Harinath Reddy , Preethi Gutha
-# Date: 12/04/2023
-# Description: The program below depicts the main file where all the classes are initialized.
